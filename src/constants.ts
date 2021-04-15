b94b591537e025339e9a7cee808c200e28fd4024
zuofenghua <11100776@bbktel.com>
2021-04-15 16:42:07 +0800

    fix: model select not work

--- src/constants.ts
+++ src/constants.ts
@@ -2,22 +2,22 @@ export const CLI_NAME = 'vitepress-dg-cli';
 export const TEMPLATE_NAME = './temp';
 
 export const TEMPLATES = [
-    {
-        name: 'vue-multiple-components',
-        value: 'doc',
-        git: 'https://github.com/dewfall123/vlib-template.git#master',
-        options: [],
-    },
-    {
-        name: 'vue-single-component',
-        value: 'doc',
-        git: 'https://github.com/dewfall123/vlib-template.git#vue-single-page',
-        options: [],
-    },
-    {
-        name: 'ts-lib',
-        value: 'doc',
-        git: 'https://github.com/dewfall123/vlib-template.git#ts-lib',
-        options: [],
-    },
+  {
+    name: 'vue-multiple-components',
+    value: 'vue-multiple-components',
+    git: 'https://github.com/dewfall123/vlib-template.git#master',
+    options: [],
+  },
+  {
+    name: 'vue-single-component',
+    value: 'vue-single-component',
+    git: 'https://github.com/dewfall123/vlib-template.git#vue-single-page',
+    options: [],
+  },
+  {
+    name: 'ts-lib',
+    value: 'ts-lib',
+    git: 'https://github.com/dewfall123/vlib-template.git#ts-lib',
+    options: [],
+  },
 ];
