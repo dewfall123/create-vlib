1c90fcce0487cdf14c55970641c1466ce2a6145d
zuofenghua <11100776@bbktel.com>
2021-04-15 15:17:42 +0800

    feat: add template options

--- src/constants.ts
+++ src/constants.ts
@@ -3,15 +3,21 @@ export const TEMPLATE_NAME = './temp';
 
 export const TEMPLATES = [
     {
-        name: 'locale',
+        name: 'vue-multiple-components',
         value: 'doc',
         git: 'https://github.com/dewfall123/vlib-template.git#master',
         options: [],
     },
-    // {
-    //     name: 'Site mode',
-    //     value: 'site',
-    //     git: 'https://github.com/dewfall123/vlib-template.git',
-    //     options: [],
-    // },
+    {
+        name: 'vue-single-component',
+        value: 'doc',
+        git: 'https://github.com/dewfall123/vlib-template.git#vue-single-page',
+        options: [],
+    },
+    {
+        name: 'ts-lib',
+        value: 'doc',
+        git: 'https://github.com/dewfall123/vlib-template.git#ts-lib',
+        options: [],
+    },
 ];
